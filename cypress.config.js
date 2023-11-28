8349fabcbbdcf0d69968d29d79c3dd64a959b04e
arulchristo.ct <arulchristo.ct@ntgfreight.com>
2023-11-28 16:32:22 +0530

    Allure Report Generation

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+///// <reference types="@shelex/cypress-allure-plugin" />
+
+const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  //reporter: 'cypress-mochawesome-reporter',
+  // overwrite: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      
+     require('cypress-mochawesome-reporter/plugin')(on);
+     allureWriter(on, config);
+      return config;
+    },
+  },
+});
